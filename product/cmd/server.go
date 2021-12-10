7b8aa4d6deb23f28b39f57946ec7282d43eb5e79
halhal <hh.25799623@gmail.com>
2021-12-10 15:54:31 +0900

    Refactor product service code

--- product/cmd/server.go
+++ product/cmd/server.go
@@ -1,43 +1,27 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
 
+	handler "github.com/halhal23/shoezoo/product/interface/grpc"
 	pb "github.com/halhal23/shoezoo/product/proto/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	fmt.Println("hello product service for shoezoo 1")
+	fmt.Println("Welcome to product service for shoezoo")
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Printf("Faild to listen: %v\n", err)
 	}
 	server := grpc.NewServer()
-	service := NewProductService()
 	reflection.Register(server)
-	pb.RegisterProductServiceServer(server, service)
+	h := handler.NewProductGrpcHandler()
+	pb.RegisterProductServiceServer(server, h)
 	if err := server.Serve(lis); err != nil {
 		log.Printf("Faild to serve: %v\n", err)
 	}
-}
-
-type ProductService struct{}
-
-func NewProductService() *ProductService {
-	return &ProductService{}
-}
-
-func (s *ProductService) ShowProduct(c context.Context, req *pb.ShowProductRequest) (*pb.ShowProductResponse, error) {
-	return &pb.ShowProductResponse{
-		Product: &pb.Product{
-			Id:    23,
-			Name:  "ジョーダン",
-			Price: 12000,
-		},
-	}, nil
 }
