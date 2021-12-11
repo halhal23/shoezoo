fbed1aeb56c38c7ba49ebf0a97ccdfc2827fc394
halhal <hh.25799623@gmail.com>
2021-12-11 12:56:22 +0900

    Add logger for ProductService

--- product/interface/grpc/handler/handler.go
+++ product/interface/grpc/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"context"
+	"log"
 
 	pb "github.com/halhal23/shoezoo/product/proto/v1"
 )
@@ -14,6 +15,7 @@ func NewProductGrpcHandler() *ProductGrpcHandler {
 
 func (s *ProductGrpcHandler) ShowProduct(c context.Context, req *pb.ShowProductRequest) (*pb.ShowProductResponse, error) {
 	id := req.GetId()
+	log.Printf("Get Request ShowProduct: id=%v", id)
 	return &pb.ShowProductResponse{
 		Product: &pb.Product{
 			Id:    id,
